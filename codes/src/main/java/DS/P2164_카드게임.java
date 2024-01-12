fcdc36dea9ec5819893032e6820756d2a1d998b4
권장순 <jsoonworld@gmail.com>
2024-01-12 17:13:27 +0900

    feat: P2164_카드게임 구현.

---
+++ codes/src/main/java/DS/P2164_카드게임.java
@@ -0,0 +1,19 @@
+package DS;
+
+
+import java.util.*;
+public class P2164_카드게임{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        Queue<Integer> queue = new LinkedList<>();
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++){ // 카드를 큐에 저장하기
+            queue.add(i);
+        }
+        while(queue.size() > 1){
+            queue.poll();
+            queue.add(queue.poll());
+        }
+        System.out.println(queue.poll()); // 마지막으로 남은 카드 출력
+    }
+}
