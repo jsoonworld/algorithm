61b5d0a7f39a98fd670b38a543e1dc6ed41625f3
권장순 <jsoonworld@gmail.com>
2024-01-16 15:18:31 +0900

    feat: (binary Search) P1300_k번째수 구현.

---
+++ codes/src/main/java/DS/search/P1300_k번째수.java
@@ -0,0 +1,33 @@
+package DS.search;
+
+import java.util.*;
+
+public class P1300_k번째수{
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        long start = 1;
+        long end = k;
+        long ans = 0;
+
+        while(start <= end){
+            long middle = (start + end) / 2;
+            long cnt = 0;
+
+            for(int i = 1; i <= n; i++){
+                cnt+= Math.min(middle/ i, n);
+            }
+            if( cnt < k){
+                start = middle + 1;
+            } else{
+                ans = middle;
+                end = middle -1;
+            }
+        }
+        System.out.print(ans);
+
+    }
+}
