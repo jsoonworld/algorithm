c78b048bc8a75a6bdab896d0a9ccbe1ba0ad7b33
권장순 <jsoonworld@gmail.com>
2024-01-18 19:02:28 +0900

    feat: (오일러 피 함수) P11689_GCDNK1 구현.

---
+++ codes/src/main/java/DS/numberTheory/eulersphi/P11689_GCDNK1.java
@@ -0,0 +1,25 @@
+package DS.numberTheory.eulersphi;
+
+import java.io.*;
+
+public class P11689_GCDNK1{
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long n = Long.parseLong(br.readLine());
+        long result  = n;
+        for(long p =2; p <= Math.sqrt(n); p++){
+            if( n % p == 0){
+                result = result - result/p;
+                while(n % p == 0){
+                    n /= p;
+                }
+            }
+        }
+        if( n > 1){
+            result = result - result /n;
+        }
+        System.out.println(result);
+    }
+
+}
