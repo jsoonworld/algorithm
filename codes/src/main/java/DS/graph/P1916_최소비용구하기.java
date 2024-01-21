3d3f614673f324f26d14e6a72ef736f7ed53f9b1
권장순 <jsoonworld@gmail.com>
2024-01-21 20:44:16 +0900

    feat: Implement Dijkstra's Algorithm for Minimum Cost Path in P1916
    이 커밋에서는 'P1916_최소비용구하기' 클래스에 다익스트라 알고리즘을 구현하여, 주어진 그래프에서 최소 비용 경로를 찾는 기능을 추가하였습니다. 이 알고리즘은 사용자로부터 그래프의 노드 수(n), 엣지 수(m), 시작점 및 도착점을 입력받아 처리합니다.
    
    주요 구현 내용:
    - 인접 리스트를 사용하여 그래프를 구현.
    - 최단 거리 배열(dist)과 방문 여부 배열(visit)을 사용하여 다익스트라 알고리즘 로직 구현.
    - 사용자 입력을 통해 시작점과 도착점을 설정하고 최소 비용 경로 계산.
    - 우선순위 큐(PriorityQueue)를 사용하여 효율적인 노드 선택 및 거리 계산.
    - 최종적으로 최소 비용 경로의 결과를 출력.

---
+++ codes/src/main/java/DS/graph/P1916_최소비용구하기.java
@@ -0,0 +1,78 @@
+package DS.graph;
+
+
+import java.util.*;
+import java.io.*;
+
+public class P1916_최소비용구하기{
+
+    static int n, m;
+    static ArrayList<Node>[] list; //인접 리스트 그래프
+    static int[] dist; // 최단 거리 배열
+    static boolean[] visit;
+
+    public static void main(String[] args) throws IOException{
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        n = Integer.parseInt(br.readLine());
+        m = Integer.parseInt(br.readLine());
+        list = new ArrayList[n+1];
+        dist = new int[n+1];
+        visit = new boolean[n+1];
+        Arrays.fill(dist, Integer.MAX_VALUE); // 거리 배열을 충분히 큰 수로 초기화.
+
+        for(int i = 1; i <= n; i++){
+            list[i] = new ArrayList<Node>(); // 각 인덱스에 ArrayList<Node> 객체 생성
+        }
+
+        for(int i = 0; i < m; i++){
+            st = new StringTokenizer(br.readLine());
+            int start = Integer.parseInt(st.nextToken());
+            int end = Integer.parseInt(st.nextToken());
+            int weight = Integer.parseInt(st.nextToken());
+            list[start].add(new Node(end, weight));
+        }
+        st = new StringTokenizer(br.readLine());
+        int startIndex = Integer.parseInt(st.nextToken());
+        int endIndex = Integer.parseInt(st.nextToken());
+        bw.write(dijkstra(startIndex, endIndex) + " \n");
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    public static int dijkstra(int start, int end){
+        PriorityQueue<Node> pq = new PriorityQueue<>();
+        pq.offer(new Node(start, 0));
+        dist[start] = 0;
+        while(!pq.isEmpty()){
+            Node nowNode = pq.poll();
+            int now = nowNode.targetNode;
+            if(!visit[now]){
+                visit[now] = true;
+                for(Node n : list[now]){
+                    if(!visit[n.targetNode] && dist[n.targetNode] > dist[now] + n.value){
+                        dist[n.targetNode] = dist[now] + n.value;
+                        pq.add(new Node(n.targetNode, dist[n.targetNode]));
+                    }
+                }
+            }
+        }
+        return dist[end];
+    }
+}
+
+class Node implements Comparable<Node>{
+    int targetNode;
+    int value;
+    Node(int targetNode, int value) {
+        this.targetNode = targetNode;
+        this.value = value;
+    }
+    @Override
+    public int compareTo(Node o) {
+        return value -o.value;
+    }
+}
