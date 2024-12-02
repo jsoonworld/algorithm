1293eef4137145000ffcfb9818fa20f0cbcbf65e
권장순 <jsoonworld@gmail.com>
2024-12-02 13:57:49 +0900

    feat : solved two pointer algo problem

---
+++ src/main/java/org/algorithm/window/p4/SubarraySum.java
@@ -0,0 +1,34 @@
+package org.algorithm.window.p4;
+
+import java.util.Scanner;
+
+public class SubarraySum {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int count = 0;
+        int sum = 0;
+        int start = 0;
+        for (int end = 0; end < n; end++) {
+            sum += arr[end];
+
+            while (sum > m) {
+                sum -= arr[start++];
+            }
+
+            if (sum == m) {
+                count++;
+            }
+        }
+
+        System.out.println(count);
+    }
+}
