f0ce6b0d8bf28d74f4c20185a5a1c5f997bbf8ed
권장순 <jsoonworld@gmail.com>
2024-12-02 15:18:51 +0900

    feat(p6) : solved two pointer algo problem

---
+++ src/main/java/org/algorithm/window/p6/LongestContinuousSubarray.java
@@ -0,0 +1,39 @@
+package org.algorithm.window.p6;
+
+import java.util.Scanner;
+
+public class LongestContinuousSubarray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        int[] arr = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println(findMaxLength(arr, k));
+    }
+
+    public static int findMaxLength(int[] arr, int k) {
+        int maxLength = 0;
+        int zeroCount = 0;
+        int left = 0;
+
+        for (int right = 0; right < arr.length; right++) {
+            if (arr[right] == 0) {
+                zeroCount++;
+            }
+
+            while (zeroCount > k) {
+                if (arr[left] == 0) {
+                    zeroCount--;
+                }
+                left++;
+            }
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+        return maxLength;
+    }
+}
