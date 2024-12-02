689ad30a1cc625cd9e43af09a19c2099a1b938b5
권장순 <jsoonworld@gmail.com>
2024-12-02 14:25:01 +0900

    feat(p5) : solved two pointer algo problem

---
+++ src/main/java/org/algorithm/window/p5/ConsecutiveSumCalculator.java
@@ -0,0 +1,35 @@
+package org.algorithm.window.p5;
+
+import java.util.Scanner;
+
+public class ConsecutiveSumCalculator {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        int count = findConsecutiveSums(n);
+        System.out.println(count);
+    }
+
+    public static int findConsecutiveSums(int n) {
+        int count = 0;
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+
+        while (start <= n / 2) {
+            if (sum == n) {
+                count++;
+                sum -= start;
+                start++;
+            } else if (sum < n) {
+                end++;
+                sum += end;
+            } else {
+                sum -= start;
+                start++;
+            }
+        }
+        return count;
+    }
+}
